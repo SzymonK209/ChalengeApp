7ea7a453d2a36c428d94c56affd94615c330572b
Szymon Kowalik <skowalik209@gmail.com>
2023-01-18 01:41:35 +0100

    zadanie domowe dzień 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string myName = "Ewa";
+char sex = 'K';
+var myAge = 33;
+
+if (sex == 'K' && myAge < 30)
+{
+    Console.WriteLine("Kobieta poniże 30 lat");
+}
+else
+{
+    if (myName == "Ewa" && sex == 'K' && myAge == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        if (sex != 'K' && myAge < 18)
+        {
+            Console.WriteLine("Niepełnoletni Mężczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Pełnoletni mężczyzna");
+        }
+    }
+}
