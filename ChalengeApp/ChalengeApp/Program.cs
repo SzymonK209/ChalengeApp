ed81a0655b0835f6c33f2521b243ee4d21507d25
Szymon Kowalik <skowalik209@gmail.com>
2023-01-19 23:56:50 +0100

    zadanie dzień 5 z *

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,38 +1,82 @@
-﻿{
-    {
-        var myName = "Ewa";
-        bool isFemale = true;
-        var myAge = 33;
+﻿using System.Diagnostics.CodeAnalysis;
 
-        if (isFemale)
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        List<string> names = new List<string>();
+        names.Add("Adam");
+        names.Add("Monika");
+        names.Add("Arek");
+        names.Add("Szymon");
+        names.Add("Agata");
+        names.Add("Adam");
+        names.Add("Amelia");
+        names.Add("Monika");
+        names.Add("Jan");
+        names.Add("Maksymilian");
+        names.Add("Adam");
+        names.Add("Nikola");
+        names.Add("Adrianna");
+        names.Add("Barbara");
+        names.Add("Monika");
+        names.Add("Adam");
+        names.Add("Wojciech");
+        names.Add("Franciszka");
+        names.Add("Roman");
+        names.Add("Adam");
+
+        var sum = 0;
+        var sumA = 0;
+        var sumB = 0;
+        var sumC = 0;
+        var sumD = 0;
+        
+        for (var i = 0; i < names.Count; i++)
         {
-            if (myAge < 30)
+            Console.WriteLine(names[i]);
+        }
+
+        Console.WriteLine("--------------");
+
+        for (int i = 0; i < names.Count; i++)
+        {
+            sum++;
+        }
+        Console.WriteLine($"Łącznie jest {sum} osób na liście,");
+
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (names[i] == "Adam")
             {
-                Console.WriteLine("Kobieta poniże 30 lat");
-            }
-            else if (myAge == 33 && myName == "Ewa")
-            {
-                Console.WriteLine(myName + ", lat 33");
-            }
-            else if (myAge > 30)
-            {
-                Console.WriteLine("Pełnoletnia Kobieta");
+                sumA++;
             }
         }
-        else if (!isFemale)
+        Console.WriteLine($"imię Adam powtórzyło się {sumA} razy,");
+
+        for (int i = 0; i < names.Count; i++)
         {
-            if (myAge < 18)
+            if (names[i] == "Monika")
             {
-                Console.WriteLine("Niepełnoletni Mężczyzna");
-            }
-            else if (myAge == 36 && myName == "Szymon")
-            {
-                Console.WriteLine(myName + ", lat 36");
-            }
-            else
-            {
-                Console.WriteLine("Pełnoletni mężczyzna");
+                sumB++;
             }
         }
+        Console.WriteLine($"imię Monika powtórzyło się {sumB} razy,");
+
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (names[i] == "Arek")
+            {
+                sumC++;
+            }
+        }
+        Console.WriteLine($"imię Arek powtórzyło się {sumC} raz,");
+
+        sumD = sum - sumA - sumB - sumC;
+
+        Console.WriteLine($"{sumD} imion bez powtórzeń.");
     }
+
+
 }
