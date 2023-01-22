39c1d90bb365f882415e1d95e1e810a3c4f96807
Szymon Kowalik <skowalik209@gmail.com>
2023-01-22 11:59:11 +0100

    zadanie dzień 5

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -5,77 +5,37 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        List<string> names = new List<string>();
-        names.Add("Adam");
-        names.Add("Monika");
-        names.Add("Arek");
-        names.Add("Szymon");
-        names.Add("Agata");
-        names.Add("Adam");
-        names.Add("Amelia");
-        names.Add("Monika");
-        names.Add("Jan");
-        names.Add("Maksymilian");
-        names.Add("Adam");
-        names.Add("Nikola");
-        names.Add("Adrianna");
-        names.Add("Barbara");
-        names.Add("Monika");
-        names.Add("Adam");
-        names.Add("Wojciech");
-        names.Add("Franciszka");
-        names.Add("Roman");
-        names.Add("Adam");
+        int number = 4566;
+        string numberToString = number.ToString();
+        char[] letters = numberToString.ToArray();
+        List<char> numbers = new List<char>();
+        numbers.Add('0');
+        numbers.Add('1');
+        numbers.Add('2');
+        numbers.Add('3');
+        numbers.Add('4');
+        numbers.Add('5');
+        numbers.Add('6');
+        numbers.Add('7');
+        numbers.Add('8');
+        numbers.Add('9');
 
-        var sum = 0;
-        var sumA = 0;
-        var sumB = 0;
-        var sumC = 0;
-        var sumD = 0;
-        
-        for (var i = 0; i < names.Count; i++)
+        Console.WriteLine($"Wynik dla liczby:" + number);
+
+        foreach (var num in numbers)
         {
-            Console.WriteLine(names[i]);
-        }
-
-        Console.WriteLine("--------------");
-
-        for (int i = 0; i < names.Count; i++)
-        {
-            sum++;
-        }
-        Console.WriteLine($"Łącznie jest {sum} osób na liście,");
-
-        for (int i = 0; i < names.Count; i++)
-        {
-            if (names[i] == "Adam")
+            int count = 0;
+            foreach (var let in letters)
             {
-                sumA++;
+                if (num == let)
+                {
+                    count++;
+                }
             }
+
+            Console.WriteLine(num + "=>" + count);
         }
-        Console.WriteLine($"imię Adam powtórzyło się {sumA} razy,");
 
-        for (int i = 0; i < names.Count; i++)
-        {
-            if (names[i] == "Monika")
-            {
-                sumB++;
-            }
-        }
-        Console.WriteLine($"imię Monika powtórzyło się {sumB} razy,");
-
-        for (int i = 0; i < names.Count; i++)
-        {
-            if (names[i] == "Arek")
-            {
-                sumC++;
-            }
-        }
-        Console.WriteLine($"imię Arek powtórzyło się {sumC} raz,");
-
-        sumD = sum - sumA - sumB - sumC;
-
-        Console.WriteLine($"{sumD} imion bez powtórzeń.");
     }
 
 
